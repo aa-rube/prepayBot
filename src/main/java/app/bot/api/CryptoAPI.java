78fce9d8abad6aece2f8cde4b565b3d6a8f40124
Alex Rube <alexrube93@gmail.com>
2024-06-07 13:36:57 -0300

    mexc update some methods when orders create

--- src/main/java/app/bot/api/CryptoAPI.java
+++ src/main/java/app/bot/api/CryptoAPI.java
@@ -10,11 +10,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 public class CryptoAPI {
     private static final String apiKey ="a1355d33fa642a7392e8ba4cee83909f72b68fdf572d4ca40ea617bf3be3c2d9";
+
     public static double getPrice(String from, String to) {
         ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.readTree(getResponse(from, to)).get(to).asDouble();
         } catch (Exception e) {
+            e.printStackTrace();
             return 0.00;
         }
     }
