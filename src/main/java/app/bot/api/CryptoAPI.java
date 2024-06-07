2dcc51c3ad428500833bbe2b4a4b160578e6b484
Alex Rube <alexrube93@gmail.com>
2024-06-07 13:12:14 -0300

    mexc update some methods when orders create

--- src/main/java/app/bot/api/CryptoAPI.java
+++ src/main/java/app/bot/api/CryptoAPI.java
@@ -29,13 +29,12 @@ public class CryptoAPI {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
-        StringBuffer response = new StringBuffer();
+        StringBuilder response = new StringBuilder();
 
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
-
         return response.toString();
     }
 }
